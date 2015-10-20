e0cd9e9fa36ca0b3e43875777374363d1e5384b4
AndrewTheo <andrew.theodoridees@gmail.com>
2015-10-20 10:31:28 -0700

    first

--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,14 @@
-class NamedCow
+class NamedCow extends Cow
 {
+   public NamedCow(String name)
+   {
+   		myName = name;
+   }
 
+   public void getName()
+   {
+   	  myName ="Andrew"
+   }
+
+   public String getName(){return myName;}
 }
