b83fb499a011c126adde75ef0ea669b8b306412f
AndrewTheo <andrew.theodoridees@gmail.com>
2015-10-21 10:45:46 -0700

    new

--- NamedCow.java
+++ NamedCow.java
@@ -1,21 +1,18 @@
 class NamedCow extends Cow
 {
-   private String myType;     
-   private String mySound; 
-   private String myName;     
+   private String myName; 
 
    public NamedCow(String type, String name, String sound)
    {
-   		myType = type;
+   		
    		myName = name;
-   		mySound = sound;
+   		
    }
 
    public void NamedCow()
    {
-   	  myType = "Cow"; 
-   	  myName ="Andrew";
-   	  mySound = "Moo"; 
+   	
+   	  myName ="Andrew";	 
    }
 
    public String getName(){return myName;}
