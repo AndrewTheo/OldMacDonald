e60dc3c2bd79d01bb4be229411aaf1e361ce04ca
AndrewTheo <andrew.theodoridees@gmail.com>
2015-10-21 10:23:12 -0700

    new

--- Chick.java
+++ Chick.java
@@ -2,18 +2,35 @@ class Chick implements Animal
 {     
      //your code here
 	private String myType;     
-    private String mySound;      
-    public Chick(String type, String sound)
+    private String mySound;    
+    private String mySound1;     
+    public Chick(String type, String sound1, String sound)
     {         
-         myType = type;         
+         myType = type;
+         mySound1 = sound1;         
          mySound = sound;     
-    }     
+    }  
+
+ 
     public Chick()
     {         
-        myType = "Chick";         
+        myType = "Chick";  
+        mySound1 = "Cheep";       
         mySound = "Cluck";     
     }      
-    public String getSound(){return mySound;}     
+    public String getSound()
+    {
+    	int num = (int)(Math.random()*2);
+ 		if(num == 1)
+ 		{
+ 			return mySound;
+ 		}
+ 		else 
+ 		{
+ 			return mySound1;
+ 		}
+    	
+    }     
     public String getType(){return myType;} 
 
 }
--- NamedCow.java
+++ NamedCow.java
@@ -1,14 +1,24 @@
 class NamedCow extends Cow
 {
-   public NamedCow(String name)
+   private String myType;     
+   private String mySound; 
+   private String myName;     
+
+   public NamedCow(String type, String name, String sound)
    {
+   		myType = type;
    		myName = name;
+   		mySound = sound;
    }
 
-   public void getName()
+   public void NamedCow()
    {
-   	  myName ="Andrew"
+   	  myType = "Cow"; 
+   	  myName ="Andrew";
+   	  mySound = "Moo"; 
    }
 
    public String getName(){return myName;}
+   public String getSound(){return mySound;}     
+   public String getType(){return myType;} 
 }
